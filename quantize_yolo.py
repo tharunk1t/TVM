5a543eeca433c2c50b3485f329c58a1115b67cb4
THARUN <108514396+tharunk1t@users.noreply.github.com>
2025-07-01 12:12:09 +0530

    Update quantize_yolo.py

--- quantize_yolo.py
+++ quantize_yolo.py
@@ -1,1 +1,12 @@
+from onnxruntime.quantization import quantize_dynamic, QuantType
 
+def quantize_model(input_path="yolov8n.onnx", output_path="yolov8n-int8.onnx"):
+    quantize_dynamic(
+        model_input=input_path,
+        model_output=output_path,
+        weight_type=QuantType.QInt8
+    )
+    print(f"✅ Quantized model saved as {output_path}")
+
+if __name__ == "__main__":
+    quantize_model()
